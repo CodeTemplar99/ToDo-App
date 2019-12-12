532d9e9cc6bbba3e93a239c250e7d65c88cff0bf
Chiadika <udoagwac@gmail.com>
2019-12-12 15:18:04 +0100

    Implemented Add new task

--- script.js
+++ script.js
@@ -21,16 +21,50 @@ document.querySelector(".create-task").addEventListener("click", newTask)
 function newTask() {
     var see = document.querySelector(".enter-new-task-con");
     see.style.display = "block";
-
-    newTask()
 }
 
 // add task now
 document.querySelector(".add-now-button").addEventListener("click", addTaskNow)
 
-function addTaskNow() {
-    var see = document.querySelector(".enter-new-task-con");
-    see.style.display = "none";
 
-    addTaskNow()
+// function newElement() {
+//     var li = document.createElement("li");
+//     var inputValue = document.getElementById("myInput").value;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+//     if (inputValue === '') {
+//         alert("You must write something!");
+//     } else {
+//         document.getElementById("myUL").appendChild(li);
+//     }
+
+function addTaskNow() {
+    for (var i = 0; i < 1; i++) {
+
+        var inputValue = document.querySelector("#task-fill").value;
+
+        var checker_text_con = document.createElement(`div`);
+        var checker = document.createElement(`input`);
+        checker.setAttribute("type", "checkbox")
+        checker.className = "mr-3"
+
+        var del = document.createElement(`i`);
+        del.className = "fa fa-times "
+        var task_text = document.createTextNode(inputValue);
+
+        if (inputValue === '') {
+            alert("you cannot leave input empty");
+        }
+        else {
+            var li = document.createElement(`li`);
+            li.className = "list-group-item";
+            checker_text_con.append(checker, task_text);
+            li.append(checker_text_con, del);
+        }
+
+        document.querySelector(".list-group").appendChild(li);
+        var see = document.querySelector(".enter-new-task-con");
+        see.style.display = "none";
+    }
 }
+
